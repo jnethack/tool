54c1dd22d55330a6a28bef94c1f8b0631b7c8ca7
SHIRAKATA Kentaro <argrath@ub32.org>
2021-06-14 05:48:37 +0900

    epitaph: add header

--- epitaph/filter.pl
+++ epitaph/filter.pl
@@ -3,12 +3,29 @@ use strict;
 use warnings;
 
 my $f = 0;
-while(<>){
+
+open my $fr, '<', 'epitaph.base.txt';
+open my $fw, '>', 'epitaph.txt';
+
+print $fw <<'EOF'
+# NetHack 3.6  epitaph.txt       $NHDT-Date: 1524689580 2018/04/25 20:53:00 $  $NHDT-Branch: NetHack-3.6.0 $:$NHDT-Revision: 1.3 $
+# Copyright (c) 2015 by Pasi Kallinen
+# NetHack may be freely redistributed.  See license for details.
+# Epitaphs for random headstones
+#
+#
+EOF
+;
+
+while(<$fr>){
     if(substr($_, 0, 1) eq '#'){
         next;
     }
     $f = 1 - $f;
     if($f == 0){
-        print $_;
+        print $fw $_;
     }
 }
+
+close $fr;
+close $fw;
